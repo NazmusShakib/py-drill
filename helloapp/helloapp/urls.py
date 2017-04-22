7e59a8c45bd25e450b38f1e28fe6b88194a7e6cd
Nazmus Shakib <mdshuvobc@gmail.com>
2017-04-22 18:12:02 +0600

    added music app.

--- helloapp/helloapp/urls.py
+++ helloapp/helloapp/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('howdy.urls')),
+    url(r'^', include('music.urls'))
 ]
--- helloapp/music/views.py
+++ helloapp/music/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.dfdfsdsdsd
+# Create your views here.
+from django.views.generic import TemplateView
+# Add this view
+class MusicPageView(TemplateView):
+    template_name = "music.html"

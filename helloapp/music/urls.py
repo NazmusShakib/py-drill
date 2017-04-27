f4fe1e3f4e08e2f7dc5b774c5893a2f6a58e361c
Nazmus Shakib <mdshuvobc@gmail.com>
2017-04-28 02:53:10 +0600

    added new template.

--- helloapp/music/urls.py
+++ helloapp/music/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from music import views
 
 urlpatterns = [
-    url(r'^music/$', views.MusicPageView.as_view()),
+    #url(r'^music/$', views.MusicPageView.as_view()),
+    url(r'^music/$', views.index, name='allMusic'),
+    url(r'^single-music/$', views.SingleMusicPageView.as_view()),
+
+    # /music/232/
+    url(r'^music/(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- helloapp/music/views.py
+++ helloapp/music/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Album
 
 # Create your views here.
 from django.views.generic import TemplateView
 # Add this view
 class MusicPageView(TemplateView):
     template_name = "music.html"
+
+class SingleMusicPageView(TemplateView):
+    template_name = "singleMusic.html"
+
+def index(request):
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="'+ url +'">'+ album.album_title +'</a><br>'
+    return HttpResponse(html)    
+
+def indexOld(request):
+    return HttpResponse("<h1>This will be a list of all Albums</h1>")
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: "+ str(album_id) +"</h2>")

f4dfe84133f056df00e1efeeaebde7e3a93711d2
Nazmus Shakib <mdshuvobc@gmail.com>
2017-05-14 13:18:46 +0600

    refactor route.

--- helloapp/music/urls.py
+++ helloapp/music/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from music import views
+app_name = 'music'
 
 urlpatterns = [
     #url(r'^music/$', views.MusicPageView.as_view()),
     url(r'^music/$', views.index, name='allMusic'),
     url(r'^single-music/$', views.SingleMusicPageView.as_view()),
 
-    # /music/232/
+    # /music/<album_id>/
     url(r'^music/(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album_id>/favorite/
+    url(r'^music/(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
--- helloapp/music/views.py
+++ helloapp/music/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Album
@@ -36,5 +37,22 @@ def index(request):
 def indexOld(request):
     return HttpResponse("<h1>This will be a list of all Albums</h1>")
 
+# def detail(request, album_id):
+#     return HttpResponse("<h2>Details for Album id: "+ str(album_id) +"</h2>")
+
+# def detail(request, album_id):
+#     try:
+#         album = Album.objects.get(pk=album_id)
+#     except Album.DoesNotExist:
+#         raise Http404("Album does not exist")
+#     return render(request, 'music/detail.html', {'album' : album})
+
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: "+ str(album_id) +"</h2>")
+    #album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    #album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/favorite.html', {'album': album})
